15ec673ee2b10946d5d79a1b0bd9c264a18702f8
Mgajurel <gajurelkushal1994@gmail.com>
2017-05-31 23:01:19 +0545

    version 1: MFCC calcultion completed, next to -> testing phase

--- src/feature/MFCC.py
+++ src/feature/MFCC.py
@@ -1,14 +1,15 @@
 import numpy as np
+from scipy.fftpack import dct
 
 class MFCCExtractor(object):
     def __init__(self, sample_rate, signal):
         self.signal = signal
         self.sample_rate = sample_rate
-        
+
     def getOriginalSignal(self):
         return self.signal
-        
-        
+
+
     def pre_emphasis(self, coeff = 0.95):
         """This function reduces the noise in the input signal, it basically is a
         filter
@@ -20,7 +21,7 @@ class MFCCExtractor(object):
         signal = self.signal
         emphasized_signal = np.append(signal[0],signal[1:]-coeff*signal[:-1])
         return emphasized_signal
-    
+
     def framing_windowing(self, frame_size = 0.025,frame_stride = 0.01 ):
         """ This function windows and frames the given signal
         :param emphasized_signal: the output from the Emphasis filter i.e. outout from function pre_emphasis
@@ -31,7 +32,7 @@ class MFCCExtractor(object):
         """
         emphasized_signal = self.pre_emphasis()
         sample_rate = self.sample_rate
-        
+
         #framming the signal
         frame_length = int(round( frame_size * sample_rate))
         frame_step = int(round( frame_stride * sample_rate))
@@ -45,7 +46,7 @@ class MFCCExtractor(object):
         #windowing the signal
         frames *= np.hamming(frame_length)
         return frames
-    
+
     def mag_spectrum(self, NFFT = 512):
         """
         this function provides the magnitude spectrum of the windowed and framed signal
@@ -67,3 +68,35 @@ class MFCCExtractor(object):
         mag_frames = self.mag_spectrum()
         pow_frames = ((1.0 / NFFT) * ((mag_frames) ** 2))
         return pow_frames
+
+    def filter_bank(self, NFFT = 512, nfilt = 40):
+        low_freq_mel = 0
+        high_freq_mel = (2595 * np.log10(1 + (self.sample_rate / 2) / 700)) #conversion from hz to mel
+        mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+        hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+        bin = np.floor((NFFT + 1) * hz_points / self.sample_rate)
+
+        fbank = np.zeros((nfilt, int(np.floor(NFFT / 2 + 1))))
+        for m in range(1, nfilt + 1):
+            f_m_minus = int(bin[m - 1])   # left
+            f_m = int(bin[m])             # center
+            f_m_plus = int(bin[m + 1])    # right
+
+            for k in range(f_m_minus, f_m):
+                fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+            for k in range(f_m, f_m_plus):
+                fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+        filter_banks = np.dot(self.pow_spectrum(), fbank.T)
+        filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)  # Numerical Stability
+        filter_banks = 20 * np.log10(filter_banks)
+        filter_banks -= (np.mean(filter_banks, axis=0) + 1e-8)#mean normalization to improve SNR
+        return filter_banks
+
+    def get_mfcc(self, num_ceps = 12, cep_lifter = 22 ):
+        mfcc = dct(self.filter_bank(), type=2, axis=1, norm='ortho')[:, 1 : (num_ceps + 1)] # Keep 2-13
+        nframes, ncoeff = mfcc.shape
+        n = np.arange(ncoeff)
+        lift = 1 + (cep_lifter / 2) * np.sin(np.pi * n / cep_lifter)
+        mfcc *= lift
+        mfcc -= (np.mean(mfcc, axis=0) + 1e-8)#mean normalization to improve SNR
+        return mfcc
--- src/speaker-recognition.py
+++ src/speaker-recognition.py
@@ -8,7 +8,7 @@ if sys.version_info[0] < 3:
     import Tkinter as Tk
 else:
     import tkinter as Tk
-    
+
 sample_rate, signal = wavfile.read('sample_male.wav')
 mfcc = MFCCExtractor(sample_rate, signal)
 
@@ -18,39 +18,50 @@ if __name__ == "__main__":
     root = Tk.Tk()
     app =  ModelInterface(root, signal)
     app.pack(side="top", fill="both", expand=False)
-    
+
     def _quit():
         root.quit()
         root.destroy()
-    
+
     def preEmphasis():
-        app.updateSignal(mfcc.pre_emphasis(), "Pre-Emphasis", "X axis", "Y axis") 
-        
+        app.updateSignal(mfcc.pre_emphasis(), "Pre-Emphasis", "X axis", "Y axis")
+
     def framing():
-        app.updateSignal(mfcc.framing_windowing(), "Frame Window", "X axis", "Y axis") 
-        
+        app.updateSignal(mfcc.framing_windowing(), "Frame Window", "X axis", "Y axis")
+
     def magnitude_frame():
-        app.updateSignal(mfcc.mag_spectrum(), "Magniture Spectrum", "X axis", "Y axis") 
-    
+        app.updateSignal(mfcc.mag_spectrum(), "Magniture Spectrum", "X axis", "Y axis")
+
     def power_frame():
-        app.updateSignal(mfcc.pow_spectrum(), "Power Spectrum", "X axis", "Y axis") 
-    
-    #Add buttons here        
+        app.updateSignal(mfcc.pow_spectrum(), "Power Spectrum", "X axis", "Y axis")
+
+    def fbank():
+        app.updateSignal(mfcc.filter_bank(), "Filter banks", "X axis", "Y axis")
+
+    def calc_mfcc():
+        app.updateSignal(mfcc.get_mfcc(), "MFCCs", "X axis", "Y axis")
+
+    #Add buttons here
     button = Tk.Button(master=root, text='Quit', command = _quit)
     button.pack(side=Tk.RIGHT, padx=20, pady=20)
-    
+
     button = Tk.Button(master=root, text='Emphasis', command=preEmphasis)
     button.pack(side=Tk.LEFT, padx=20, pady=20)
-    
+
     button = Tk.Button(master=root, text='Frame', command=framing)
     button.pack(side=Tk.LEFT, padx=20, pady=20)
-    
+
     button = Tk.Button(master=root, text='Magnitude', command=magnitude_frame)
     button.pack(side=Tk.LEFT, padx=20, pady=20)
-    
+
     button = Tk.Button(master=root, text='Power', command=power_frame)
     button.pack(side=Tk.LEFT, padx=20, pady=20)
-    
+
+    button = Tk.Button(master=root, text='filter banks', command=fbank)
+    button.pack(side=Tk.LEFT, padx=20, pady=20)
+
+    button = Tk.Button(master=root, text='MFCC', command=calc_mfcc)
+    button.pack(side=Tk.LEFT, padx=20, pady=20)
+
 #    root.resizable(0,0)
     root.mainloop()
-
