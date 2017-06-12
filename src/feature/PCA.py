8ebb5039520c779213b98fc38e20057a5101622a
Surendra Shrestha <shresthasurendra37@gmail.com>
2017-06-12 16:55:26 +0545

    Modify: For final presentation

--- src/feature/PCA.py
+++ src/feature/PCA.py
@@ -1,24 +1,47 @@
-class PCAExtractor(object):
-    def __init__(self, fs, win_length, win_shift_ms, n_pca,
-    pre_emphasis_coefs):
-        self.PRE_EMPH = pre_emphasis_coefs
-        self.n_pca = n_pca
-        #self.n_pca = n_pca + 1
-        
-        self.FRAME_LEN = int(float(win_length_ms) / 1000 * fs)
-        self.FRAME_SHIFT = int(float(win_shift_ms) / 1000 * fs)
-        self.window = hamming(self.FRAME_LEN)
-        
-    # function to convert pca to cepstrum coefficient
-    def pca_to_cc(self, pca):
-        pass
-        
-    def pcac(self, signal):
-        pass
-        
-        
-    #Function to extract pca from given signal    
-    def extract(self, signal):
-        pass
-        
-        
+from numpy import array, dot, mean, std, empty, argsort
+from numpy.linalg import eigh, solve
+from numpy.random import randn
+from matplotlib.pyplot import subplots, show
+
+def cov(data):
+    """
+    Covariance matrix
+    note: specifically for mean-centered data
+    note: numpy's `cov` uses N-1 as normalization
+    """
+    return dot(self.T, self) / self.shape[0]
+    # N = data.shape[1]
+    # C = empty((N, N))
+    # for j in range(N):
+    #   C[j, j] = mean(data[:, j] * data[:, j])
+    #   for k in range(j + 1, N):
+    #       C[j, k] = C[k, j] = mean(data[:, j] * data[:, k])
+    # return C
+
+def pca(data, pc_count = None):
+    """
+    Principal component analysis using eigenvalues
+    note: this mean-centers and auto-scales the data (in-place)
+    """
+    data -= mean(data, 0)
+    data /= std(data, 0)
+    C = cov(data)
+    E, V = eigh(C)
+    key = argsort(E)[::-1][:pc_count]
+    E, V = E[key], V[:, key]
+    U = dot(data, V)  # used to be dot(V.T, data.T).T
+    return U, E, V
+
+""" test data """
+data = array([randn(8) for k in range(150)])
+data[:50, 2:4] += 5
+data[50:, 2:5] += 5
+
+""" visualize """
+trans = pca(data, 3)[0]
+fig, (ax1, ax2) = subplots(1, 2)
+ax1.scatter(data[:50, 0], data[:50, 1], c = 'r')
+ax1.scatter(data[50:, 0], data[50:, 1], c = 'b')
+ax2.scatter(trans[:50, 0], trans[:50, 1], c = 'r')
+ax2.scatter(trans[50:, 0], trans[50:, 1], c = 'b')
+show()
--- src/speaker-recognition.py
+++ src/speaker-recognition.py
@@ -12,45 +12,16 @@ else:
 
 sample_rate, signal = wavfile.read('vf3-20.wav')
 
-
-mfcc = MFCCExtractor(sample_rate, signal)
+mfcc = MFCCExtractor(fs=sample_rate)
 
 
 def init_widget():
 	#Add buttons here
-    button = Tk.Button(master=root, text='Original', command = original)
-    button.pack(side=Tk.LEFT, padx=20)
-
-    button = Tk.Button(master=root, text='Silence', command = silence)
+    button = Tk.Button(master=root, text='MFCC', command=calc_mfcc)
     button.pack(side=Tk.LEFT, padx=20)
 
     button = Tk.Button(master=root, text='Quit', command = _quit)
     button.pack(side=Tk.RIGHT, padx=20)
-
-    button = Tk.Button(master=root, text='Emphasis', command=preEmphasis)
-    button.pack(side=Tk.LEFT, padx=20)
-
-    button = Tk.Button(master=root, text='Frame', command=framing)
-    button.pack(side=Tk.LEFT, padx=20)
-
-    button = Tk.Button(master=root, text='Magnitude', command=magnitude_frame)
-    button.pack(side=Tk.LEFT, padx=20)
-
-    button = Tk.Button(master=root, text='Power', command=power_frame)
-    button.pack(side=Tk.LEFT, padx=20)
-
-    button = Tk.Button(master=root, text='filter banks', command=fbank)
-    button.pack(side=Tk.LEFT, padx=20)
-
-
-    button = Tk.Button(master=root, text='frames from filter banks', command=feat_after_Fbank)
-    button.pack(side=Tk.LEFT, padx=20)
-
-
-    button = Tk.Button(master=root, text='MFCC', command=calc_mfcc)
-    button.pack(side=Tk.LEFT, padx=20)
-
-
 
 if __name__ == "__main__":
     root = Tk.Tk()
@@ -61,42 +32,10 @@ if __name__ == "__main__":
         root.quit()
         root.destroy()
 
-    def preEmphasis():
-        app.updateSignal(mfcc.pre_emphasis(), "Pre-Emphasis", "X axis", "Y axis")
-
-    def framing():
-        app.updateSignal(mfcc.framing_windowing(), "Frame Window", "X axis", "Y axis")
-
-    def magnitude_frame():
-        app.updateSignal(mfcc.mag_spectrum(), "Magniture Spectrum", "X axis", "Y axis")
-
-    def power_frame():
-        app.updateSignal(mfcc.pow_spectrum(), "Power Spectrum", "X axis", "Y axis")
-
-    def fbank():
-        app.updateSignal(mfcc.get_filterbanks(), "Filter banks", "X axis", "Y axis")
-
-    def feat_after_Fbank():
-        feat, energy = mfcc.filter_bank()
-        app.updateSignal(feat,"After passing to Fbank","X axis","Y axis")
-
-    def calc_mfcc():
-        
-        app.updateSignal(mfcc.get_mfcc(), "MFCCs", "X axis", "Y axis")
-
-    def original():
-      mfcc.set_signal(signal)
-      app.updateSignal(mfcc.get_signal(), "MFCCs", "X axis", "Y axis")
-
-
-    def silence():
-    	mfcc.set_signal(remove_silence(sample_rate, signal))
-    	app.updateSignal(mfcc.get_signal(), "Silenced", "X axis", "Y axis")
-
+    def calc_mfcc():        
+        app.updateSignal(mfcc.extract(signal), "MFCCs", "X axis", "Y axis")
 
     init_widget()
-
-
 
 #    root.resizable(0,0)
     root.mainloop()
