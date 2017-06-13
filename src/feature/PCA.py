2b6b6d93305a3e8b60776d79441949d9c36859c9
Surendra Shrestha <shresthasurendra37@gmail.com>
2017-06-13 12:10:48 +0545

    Added PCA.py

--- src/feature/PCA.py
+++ src/feature/PCA.py
@@ -1,47 +1,12 @@
-from numpy import array, dot, mean, std, empty, argsort
-from numpy.linalg import eigh, solve
-from numpy.random import randn
-from matplotlib.pyplot import subplots, show
+#PCA-Principal Component Analysis
+#To reduce the dimensions of the feature vectors obtained from MFCC
+#Author: Surendra Shrestha
+#Date: 2017, June-13
+#GitHub username: suren37
 
-def cov(data):
-    """
-    Covariance matrix
-    note: specifically for mean-centered data
-    note: numpy's `cov` uses N-1 as normalization
-    """
-    return dot(self.T, self) / self.shape[0]
-    # N = data.shape[1]
-    # C = empty((N, N))
-    # for j in range(N):
-    #   C[j, j] = mean(data[:, j] * data[:, j])
-    #   for k in range(j + 1, N):
-    #       C[j, k] = C[k, j] = mean(data[:, j] * data[:, k])
-    # return C
+class PCA(object):
+    def __init__(self):
+        pass
 
-def pca(data, pc_count = None):
-    """
-    Principal component analysis using eigenvalues
-    note: this mean-centers and auto-scales the data (in-place)
-    """
-    data -= mean(data, 0)
-    data /= std(data, 0)
-    C = cov(data)
-    E, V = eigh(C)
-    key = argsort(E)[::-1][:pc_count]
-    E, V = E[key], V[:, key]
-    U = dot(data, V)  # used to be dot(V.T, data.T).T
-    return U, E, V
-
-""" test data """
-data = array([randn(8) for k in range(150)])
-data[:50, 2:4] += 5
-data[50:, 2:5] += 5
-
-""" visualize """
-trans = pca(data, 3)[0]
-fig, (ax1, ax2) = subplots(1, 2)
-ax1.scatter(data[:50, 0], data[:50, 1], c = 'r')
-ax1.scatter(data[50:, 0], data[50:, 1], c = 'b')
-ax2.scatter(trans[:50, 0], trans[:50, 1], c = 'r')
-ax2.scatter(trans[50:, 0], trans[50:, 1], c = 'b')
-show()
+    def pca(self):
+        pass
